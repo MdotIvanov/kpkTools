ec23aa228581b191d6c7bcc3322d579ce69cb951
MdotIvanov <MdotIvanov@gmail.com>
2014-06-28 14:47:12 +0300

    homework done

---
+++ Template/Program.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Program
+    {
+        private static readonly ILog log =
+    LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+
+            BasicConfigurator.Configure();
+            log.Debug("Debug msg");
+            log.Error("Error msg");
+        }
+    }
+}
